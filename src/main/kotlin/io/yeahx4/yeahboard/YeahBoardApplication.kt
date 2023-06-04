8a9f8ab9e0d28e3c206b053c3f29c15bb0987195
YEAHx4 <developerstarlight@gmail.com>
2023-06-04 12:04:46 +0900

    Init Kotlin spring boot project

---
+++ src/main/kotlin/io/yeahx4/yeahboard/YeahBoardApplication.kt
@@ -0,0 +1,11 @@
+package io.yeahx4.yeahboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YeahBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<YeahBoardApplication>(*args)
+}
---
+++ src/test/kotlin/io/yeahx4/yeahboard/YeahBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.yeahx4.yeahboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YeahBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
